76000802cfb0e984c1d630e8145f763f2ad7053f
Alexander Marquardt <alexander.marquardt@gmail.com>
2014-06-09 16:10:19 -0400

    Figured out how to import dev_appserver into the run script

--- run_app.py
+++ run_app.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python
 
 
-import vidsetup,  subprocess
+import vidsetup,  subprocess, sys
 
-setup.customize_files()
+sys.path.append("/usr/local/google_appengine")
 
+    
+# Execute the code for generating css, compressing, minimizing, watching for modifications, etc.   
+# This comes before the dev_appserver call because that call runs infinitely
+vidsetup.customize_files()
 
-pargs = ['old_dev_appserver.py', "."]
-subprocess.call(pargs ,  stderr=subprocess.STDOUT)  
+if vidsetup.DEBUG_BUILD:
+    from old_dev_appserver import *
+    run_file("/usr/local/google_appengine/old_dev_appserver.py", globals())
+
+else: 
+    from dev_appserver import *
+    _run_file("/usr/local/google_appengine/dev_appserver.py", globals())
+    
+
