730dfc4520a194798ece811ac371ef61466011c6
Alexander Marquardt <alexander.marquardt@gmail.com>
2014-07-08 15:03:31 -0400

    Added constants declarations to videoApp

--- client/app/scripts/ng_controllers/mainVideoController.js
+++ client/app/scripts/ng_controllers/mainVideoController.js
@@ -3,6 +3,16 @@
  */
 "use strict";
 
+
+/* jshint ignore: start */
+angular.module("videoApp")
+    .constant("errorMessages", errorMessages)
+    .constant("channelToken", channelToken)
+    .constant("me", me)
+    .constant("roomKey", roomKey)
+    .constant("initiator", initiator);
+/* jshint ignore: end */
+
 angular.module('videoApp')
 .controller('mainVideoCtrl', function ($scope) {
 
