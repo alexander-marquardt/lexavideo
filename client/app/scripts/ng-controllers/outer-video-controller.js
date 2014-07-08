76c51d41df79d67d6f997e783f6208b4a1d806a0
Alexander Marquardt <alexander.marquardt@gmail.com>
2014-07-08 08:23:58 -0400

    wrapped html with ng-app and ng-controller

---
+++ client/app/scripts/ng-controllers/outer-video-controller.js
@@ -0,0 +1,10 @@
+/**
+ * Created by alexandermarquardt on 2014-07-08.
+ */
+
+"use strict";
+
+angular.module('videoApp')
+.controller('outerVideoCtrl', function ($scope) {
+
+});
---
+++ client/app/scripts/ng-services/video-app-constants.js
@@ -0,0 +1,13 @@
+/**
+ * Created by alexandermarquardt on 2014-07-08.
+ */
+
+
+/* jshint ignore: start */
+angular.module("videoApp")
+    .constant("errorMessages", errorMessages)
+    .constant("channelToken", channelToken)
+    .constant("me", me)
+    .constant("roomKey", roomKey)
+    .constant("initiator", initiator);
+/* jshint ignore: end */
