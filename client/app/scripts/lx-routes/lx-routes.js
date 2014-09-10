b9cad8588b875c113f5f63ee052332cb33eeb251
Alexander Marquardt <alexander.marquardt@gmail.com>
2014-09-10 09:37:29 -0400

    Figured out how to pass URL parameter to server

--- client/app/scripts/lx-routes/lx-routes.js
+++ client/app/scripts/lx-routes/lx-routes.js
@@ -13,9 +13,15 @@ var getServerConstantsCtrl = lxMainRoutes.controller('getServerConstantsCtrl', f
 getServerConstantsCtrl.resolve = {
 
     // the following
-    serverConstants: function($q, $http) {
+    serverConstants: function($q, $http, $route) {
         var deferred = $q.defer();
+        var roomName = $route.current.params['roomName'];
         var url = '/json/get_video_params';
+        if (roomName) {
+            url = url + '/' + roomName;
+        } else {
+            $log.error('Error: attempting to call ' + url + 'without a roomName parameter.');
+        }
         $http({method: 'GET', url: url})
             .success(function(data) {
                 deferred.resolve(data)
@@ -31,6 +37,7 @@ getServerConstantsCtrl.resolve = {
 lxMainRoutes.config(function ($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
 
+
     $routeProvider.when('/', {
         templateUrl: '/_jx/lx-templates/lx-welcome.html'
     });
@@ -40,6 +47,8 @@ lxMainRoutes.config(function ($routeProvider, $locationProvider) {
         controller: 'getServerConstantsCtrl',
         resolve: getServerConstantsCtrl.resolve
     });
+
+
 
     $routeProvider.otherwise({
         templateUrl: '/_jx/lx-templates/lx-welcome.html'
