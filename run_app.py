40d31470381ddf5399736778b628dd8874c6cefc
Alexander Marquardt <alexander.marquardt@gmail.com>
2014-09-16 02:47:44 -0400

    Got the new dev_appserver working in run_app.py -- there should not be any reason to use the old_dev_appserver any longer.

--- run_app.py
+++ run_app.py
@@ -4,14 +4,15 @@ from build_support import prepare_build
 import sys
 import vidsetup
 
-sys.path.append("/usr/local/google_appengine")
-
 # Execute the code for generating css, compressing, minimizing, watching for modifications, etc.   
 # This comes before the dev_appserver call because that call runs infinitely
 prepare_build.customize_files(version_id = vidsetup.VERSION_ID)
 
-
 if __name__ == "__main__":
-    # if this is being called as a python script then run the developement server
-    from old_dev_appserver import *
-    run_file("/usr/local/google_appengine/old_dev_appserver.py", globals())
+    sys.path.append("/usr/local/google_appengine")    
+    import dev_appserver
+    
+    # The following hack makes it appear that we are running the dev_appserver from the /usr/local/google_appengine directory,
+    # which is required for the script to run properly.
+    sys.argv[0] = "/usr/local/google_appengine/dev_appserver.py"
+    dev_appserver._run_file("/usr/local/google_appengine/dev_appserver.py", globals())
