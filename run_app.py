dea4e142d55fecb4bafd1e0358a00f5e30261ee9
Alexander Marquardt <alexander.marquardt@gmail.com>
2014-09-16 14:54:10 -0400

    Added command line option for determining which version of the dev_appserver will be executed.

--- run_app.py
+++ run_app.py
@@ -10,9 +10,17 @@ prepare_build.customize_files(version_id = vidsetup.VERSION_ID)
 
 if __name__ == "__main__":
     sys.path.append("/usr/local/google_appengine")    
-    import dev_appserver
     
     # The following hack makes it appear that we are running the dev_appserver from the /usr/local/google_appengine directory,
     # which is required for the script to run properly.
-    sys.argv[0] = "/usr/local/google_appengine/dev_appserver.py"
-    dev_appserver._run_file("/usr/local/google_appengine/dev_appserver.py", globals())
+    
+    if '--use_old_dev_appserver' in sys.argv:
+        # remove this option from sys.argv so that it doesn't cause problems in other parts of the code
+        sys.argv = filter(lambda a: a != "--use_old_dev_appserver", sys.argv)
+        
+        import old_dev_appserver 
+        old_dev_appserver.run_file("/usr/local/google_appengine/old_dev_appserver.py", globals())    
+    else:
+        import dev_appserver        
+        sys.argv[0] = "/usr/local/google_appengine/dev_appserver.py"
+        dev_appserver._run_file("/usr/local/google_appengine/dev_appserver.py", globals())        
