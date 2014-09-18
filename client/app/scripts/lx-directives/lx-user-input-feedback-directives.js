2e6d33244a39f42d570e2b6ed60324be429ce018
Alexander Marquardt <alexander.marquardt@gmail.com>
2014-09-18 15:49:47 -0400

    Modified the code that watches room status and provides user feedback.

--- client/app/scripts/lx-directives/lx-user-input-feedback-directives.js
+++ client/app/scripts/lx-directives/lx-user-input-feedback-directives.js
@@ -3,7 +3,7 @@
 
 angular.module('lxUserInputFeedback.directives', [])
 
-    .directive('ensureUnique', function(lxHandleRoomService) {
+    .directive('checkForUniqueRoomName', function($parse, lxHandleRoomService) {
 
         var maxOccupancy = 2;
 
@@ -11,21 +11,36 @@ angular.module('lxUserInputFeedback.directives', [])
             require: 'ngModel',
             link: function(scope, elem, attrs, ctrl) {
 
-                    scope.$watch(attrs.ngModel, function(newVal) {
-                        var roomObj = null;
-                        if (newVal) {
-                            roomObj = lxHandleRoomService.getRoom(newVal);
-                        }
+                var inputElement = angular.element(elem)[0];
 
-                        roomObj && roomObj.$promise.then(function(data) {
-                            if (data.roomOccupancy == maxOccupancy) {
-                                ctrl.$setValidity('validRoom', false);
-                            } else {
-                                ctrl.$setValidity('validRoom', true);
+                scope.$watch(function() {
+                        // We watch the input element directly because the ngModel is only set when all validity
+                        // conditions are met -- but this would not allow us to reset the validRoom variable
+                        // if the user hits the backspace key and enters in a room name that is too short
+                        // after previously entering in an invalid room name.
+                        return inputElement.value
+                    },
+                    function(newVal) {
+                        var roomObj = null;
+
+                        ctrl.$setValidity('validRoom', true)
+                        if (ctrl.$valid) {
+                            if (newVal) {
+                                roomObj = lxHandleRoomService.getRoom(newVal);
                             }
-                            //ctrl.$setValidity('numInRoom', data.roomOccupancy)
-                        })
-                    });
+
+                            roomObj && roomObj.$promise.then(function(data) {
+                                if (data.roomOccupancy == maxOccupancy) {
+                                    ctrl.$setValidity('validRoom', false);
+                                } else {
+                                    ctrl.$setValidity('validRoom', true);
+                                }
+                                //ctrl.$setValidity('numInRoom', data.roomOccupancy)
+                            })
+                        } 
+                    }
+                );
             }
         }
     });
+
