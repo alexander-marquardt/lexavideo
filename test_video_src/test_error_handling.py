0c3ed9fd9953b1015dfdfb906c53810da917925b
Alexander Marquardt <alexander.marquardt@gmail.com>
2014-09-23 17:12:56 -0400

    Added some additional unit test verifications

--- test_video_src/test_error_handling.py
+++ test_video_src/test_error_handling.py
@@ -11,7 +11,6 @@ exception_string = "This function had an exception foobar!!"
 
 @error_handling.handle_exceptions
 def handle_exceptions_wrapped_fn_throws_error():
-
     #intentionally throw an error so that the handle_exceptions function will be tested
     raise Exception(exception_string)
 
@@ -19,6 +18,7 @@ def handle_exceptions_wrapped_fn_throws_error():
 class WebAppHandler(webapp2.RequestHandler):
     @error_handling.handle_exceptions
     def get(self):
+        #intentionally throw an error so that the handle_exceptions function will be tested
         raise Exception(exception_string)
 
 
@@ -44,10 +44,11 @@ class TestErrorHandlingHandleExceptions(unittest.TestCase):
         self.check_stderr_log(mock_logging_error.call_args[0][0])
 
 
+    # For some strange reason, in the PyCharm debugger, this test will not execute, but in WingIDE and
+    # from the command line it executes properly.
     @patch('error_handling.logging.debug')
     @patch('error_handling.logging.error')
     def test_webapp_handler_errors(self, mock_logging_error, mock_logging_debug):
-
         app = webapp2.WSGIApplication([('/', WebAppHandler)])
         response = app.get_response('/')
 
@@ -56,7 +57,13 @@ class TestErrorHandlingHandleExceptions(unittest.TestCase):
 
         self.assertTrue(mock_logging_debug.called)
         self.assertTrue(mock_logging_error.called)
-        self.check_stderr_log(mock_logging_error.call_args[0])
+        
+        debug_called_with = mock_logging_debug.call_args_list[0][0][0]
+        self.assertRegexpMatches(debug_called_with, 'executing set_http_error_json_response')
+        
+        debug_called_with = mock_logging_debug.call_args_list[1][0][0]
+        self.assertRegexpMatches(debug_called_with, 'executing log_call_stack_and_traceback with extra_info = serverError')
+        self.check_stderr_log(mock_logging_error.call_args[0][0])
 
 if __name__ == "__main__":
     unittest.main()
