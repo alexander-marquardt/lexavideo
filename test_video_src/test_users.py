2a8be7b8c4d49b855943363facc100e5ea195808
Alexander Marquardt <alexander.marquardt@gmail.com>
2014-09-26 14:09:20 -0400

    New structure and associated tests for storing user data

---
+++ test_video_src/test_users.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+import unittest
+
+from google.appengine.ext import testbed
+
+import setup_sys_path_for_testing
+
+
+# Test the basic functionality in the users module
+
+from video_src import users
+
+class TestUtils(unittest.TestCase):
+
+    new_user_name = None
+
+    def setUp(self):
+        # First, create an instance of the Testbed class.
+        self.testbed = testbed.Testbed()
+        # Then activate the testbed, which prepares the service stubs for use.
+        self.testbed.activate()
+        # Next, declare which service stubs you want to use.
+        self.testbed.init_datastore_v3_stub()
+        self.testbed.init_memcache_stub()
+
+    def tearDown(self):
+        self.testbed.deactivate()
+
+    def step1(self):
+        user_obj = users.create_new_user()
+        self.assertIsNotNone(user_obj)
+        self.assertEqual(user_obj.user_name, str(user_obj.key.id()))
+        self.new_user_name = user_obj.user_name
+
+    def step2(self):
+        user_obj = users.get_user(int(self.new_user_name))
+        self.assertEqual(user_obj.user_name, str(user_obj.key.id()))
+
+    def step3(self):
+        users.delete_user(int(self.new_user_name))
+        user_obj = users.get_user(self.new_user_name)
+        self.assertIsNone(user_obj)
+
+    def test_create_get_and_delete_user(self):
+        self.step1()
+        self.step2()
+        self.step3()
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ video_src/users.py
@@ -0,0 +1,25 @@
+
+
+from video_src import models
+
+def create_new_user():
+
+    new_user_obj = models.UserModel()
+    new_user_obj.put()
+
+    # use the key as the username until they decide to create their own username.
+    new_user_name = new_user_obj.key.id()
+    new_user_obj.user_name = str(new_user_name)
+    new_user_obj.put()
+    return new_user_obj
+
+def get_user(user_id):
+    # queries database for user and returns the user object
+    user_obj = models.UserModel.get_by_id(user_id)
+    return user_obj
+
+def delete_user(user_id):
+    # removes  particular user from the database
+    user_obj = models.UserModel.get_by_id(user_id)
+    user_obj.key.delete()
+
