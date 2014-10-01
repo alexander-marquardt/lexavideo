321b60016f92c66f14b717edc431228d350654f4
Alexander Marquardt <alexander.marquardt@gmail.com>
2014-10-01 18:47:17 -0400

    minor variable renaming

--- video_src/messaging.py
+++ video_src/messaging.py
@@ -6,44 +6,45 @@ from google.appengine.api import channel
 
 from video_src import models
 
-def handle_message(room_obj, user_id, message):
+def handle_message(room_obj, from_user_id, message):
     # This function passes a message from one user in a given "room" to the other user in the same room.
     # It is used for exchanging sdp (session description protocol) data for setting up sessions, as well
     # as for passing video and other information from one user to the other. 
 
     message_obj = json.loads(message)
     message = message.decode("utf-8")
-    other_user_id = room_obj.get_other_user_id(user_id)
+    to_user_id = room_obj.get_other_user_id(from_user_id)
     room_name = room_obj.room_name
 
     message_type = message_obj['messageType']
     message_payload = message_obj['messagePayload']
 
-    if message_type == 'sdp' and message_payload['type'] == 'bye':
-        # This would remove the other_user in loopback test too.
-        # So check its availability before forwarding Bye message.
-        room_obj.remove_user(user_id)
-        logging.info('User %d ' % user_id + ' quit from room ' + room_name)
-        logging.info('Room ' + room_name + ' has state ' + repr(room_obj))
+    if message_type == 'sdp':
+        if message_payload['type'] == 'bye':
+            # This would remove the other_user in loopback test too.
+            # So check its availability before forwarding Bye message.
+            room_obj.remove_user(from_user_id)
+            logging.info('User %d ' % from_user_id + ' quit from room ' + room_name)
+            logging.info('Room ' + room_name + ' has state ' + repr(room_obj))
 
     if message_type == 'videoSettings':
-        logging.info('***** videoSettings message received: ' + repr(message_payload))
+        logging.info('videoSettings message received: ' + repr(message_payload))
 
 
-    if other_user_id and room_obj.has_user(other_user_id):
+    if to_user_id and room_obj.has_user(to_user_id):
         if message_type == 'sdp' and message_payload['type'] == 'offer':
             # This is just for debugging
             logging.info('sdp offer. Payload: %s' % repr(message_payload))
 
-        if message_type == 'sdp' and message_payload['type'] == 'offer' and other_user_id == user_id:
+        if message_type == 'sdp' and message_payload['type'] == 'offer' and to_user_id == from_user_id:
             # Special case the loopback scenario.
             #message = make_loopback_answer(message)
             pass
 
-        on_message(room_obj, other_user_id, message)
+        on_message(room_obj, to_user_id, message)
 
     else:
-        logging.warning('Cannot deliver message from user: %s to other_user: %s since they are not in the room: %s' % (user_id, other_user_id, room_name))
+        logging.warning('Cannot deliver message from user: %s to other_user: %s since they are not in the room: %s' % (from_user_id, to_user_id, room_name))
         # For unittest
         #on_message(room, user, message)
 
