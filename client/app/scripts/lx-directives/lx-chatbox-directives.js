6c688ff357621defe71e52d076a7500dbf52f30c
Alexander Marquardt <alexander.marquardt@gmail.com>
2014-10-30 10:54:47 -0400

    Chatbox directive is now able to append messages into the chat panel

--- client/app/scripts/lx-directives/lx-chatbox-directives.js
+++ client/app/scripts/lx-directives/lx-chatbox-directives.js
@@ -12,12 +12,29 @@ angular.module('lxChatbox.directives', [])
     function() {
         return {
             restrict: 'A',
-            templateUrl: 'lx-template-cache/wrapped-chat-message.html',
-            link: function (scope) {
+            link: function (scope, elem) {
+
+                var addMessageToDisplay = function(message) {
+
+                    var messageHtml = angular.element('<div  class="row cl-chat-row">');
+
+                    messageHtml.append(angular.element('<div class="col-xs-3 col-md-2 text-left">')
+                            .append('<span class="icon-lx-chat-bubble-left-cfg clearfix">')
+                            .append(angular.element('<small class="text-muted clearfix"><span class="icon-lx-time">')
+                                .append('time')
+                        )
+                    );
+                    messageHtml.append(angular.element('<div class="col-xs-9 col-md-10 chat-body text-right">').append(message));
+
+                    elem.append(messageHtml);
+                    elem.append('<div class="cl-chat-divider">');
+
+
+                };
 
                 // watch to see if the local user has sent a new chat message to the remote user
                 scope.$watch('sendMessageString', function(newValue) {
-                    scope.chatMessage = newValue;
+                    addMessageToDisplay(newValue);
                 });
             }
         };
