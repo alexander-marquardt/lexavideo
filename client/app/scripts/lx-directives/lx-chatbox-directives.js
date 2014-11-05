ea04d8df09158a59aad7af5620e276d5d52c3469
Alexander Marquardt <alexander.marquardt@gmail.com>
2014-11-05 02:03:25 -0500

    Moved "private" functions into the private area

--- client/app/scripts/lx-directives/lx-chatbox-directives.js
+++ client/app/scripts/lx-directives/lx-chatbox-directives.js
@@ -3,7 +3,7 @@
  */
 'use strict';
 
-
+/* global $ */
 
 angular.module('lxChatbox.directives', [])
 
@@ -16,13 +16,79 @@ angular.module('lxChatbox.directives', [])
         ) {
 
         var flashChatboxNotificationTime = 500; //ms
+        var windowFocus;
+        var numMessagesReceivedSinceWindowLastActive = 0;
+        var timerId;
+
+        // function that removes the message counter from the document title, and resets the
+        // message counter back to zero.
+        var resetDocumentTitleToDefault = function() {
+            numMessagesReceivedSinceWindowLastActive = 0;
+            document.title = $('#id-document-title-div').text();
+            // remove blinking of the number of messages
+            $timeout.cancel(timerId);
+        };
+
+        // self-executing function that monitors the window to see if it has focus, and set the windowFocus
+        // variable appropriately
+        (function setWindowFocus() {
+            $(window).focus(function() {
+                windowFocus = true;
+                resetDocumentTitleToDefault();
+            }).blur(function() {
+                windowFocus = false;
+            });
+        })();
+
+        // Displays the number of messages received in the document title , and flashes the
+        // number of messages to get the users attention.
+        var showNumMessagesInDocumentTitle = function() {
+            var numMessagesIsShownToggle = true;
+            // if the user is not looking at the current window, then show them how many messages
+            // they have missed while they were not paying attention.
+            if (!windowFocus) {
+                document.title = '(' + numMessagesReceivedSinceWindowLastActive + ') ' + $('#id-document-title-div').text();
+
+                if (timerId) {
+                    // a previous timer was running, so we remove it.
+                    $timeout.cancel(timerId);
+                }
+
+                // don't start flashing until 10 seconds have passed.
+                var timeoutDelay = 10000;
+                // the following timer is used for switching between the title with and without the number of
+                // new messages included in the title.
+                var timeoutFn = function () {
+                    timerId = $timeout(function () {
+                        if (numMessagesIsShownToggle) {
+                            document.title = $('#id-document-title-div').text();
+
+                        } else {
+                            // the arrow is now shown, display it for a while
+                            document.title = '(' + numMessagesReceivedSinceWindowLastActive + ') ' + $('#id-document-title-div').text();
+                        }
+                        numMessagesIsShownToggle = !numMessagesIsShownToggle;
+                        // after initial wait, start flashing every 2 seconds.
+                        timeoutDelay = 1000;
+
+                        timeoutFn();
+                    }, timeoutDelay);
+                };
+                timeoutFn();
+            }
+            // otherwise the user is currently looking at the window, and so we make sure that the
+            // display does not show number of messages and that the timer and counter are reset.
+            else {
+                resetDocumentTitleToDefault();
+            }
+        };
+
 
         return {
             restrict: 'A',
             link: function (scope, elem) {
 
-                var window_focus;
-                var numMessagesReceivedSinceWindowLastActive = 0;
+
                 var chatPanelBody = angular.element(elem).parent();
                 var chatPanelHeadingElement = chatPanelBody.prev();
                 var chatPanel = chatPanelBody.parent();
@@ -73,71 +139,6 @@ angular.module('lxChatbox.directives', [])
                 };
 
 
-                // function that removes the message counter from the document title, and resets the
-                // message counter back to zero.
-                var resetDocumentTitleToDefault = function() {
-                    numMessagesReceivedSinceWindowLastActive = 0;
-                    document.title = $('#id-document-title-div').text();
-                    // remove blinking of the number of messages
-                    $timeout.cancel(timerId);
-                };
-
-
-                // self-executing function that monitors the window to see if it has focus, and set the window_focus
-                // variable appropriately
-                (function setWindowFocus() {
-                    $(window).focus(function() {
-                        window_focus = true;
-                        resetDocumentTitleToDefault();
-                    }).blur(function() {
-                        window_focus = false;
-                    });
-                })();
-
-
-                // Displays the number of messages received in the document title , and flashes the
-                // number of messages to get the users attention.
-                var showNumMessagesInDocumentTitle = function() {
-                    var numMessagesIsShownToggle = true;
-                    // if the user is not looking at the current window, then show them how many messages
-                    // they have missed while they were not paying attention.
-                    if (!window_focus) {
-                        document.title = '(' + numMessagesReceivedSinceWindowLastActive + ') ' + $('#id-document-title-div').text();
-
-                        if (timerId) {
-                            // a previous timer was running, so we remove it.
-                            $timeout.cancel(timerId);
-                        }
-
-                        // don't start flashing until 10 seconds have passed.
-                        var timeoutDelay = 10000;
-                        // the following timer is used for switching between the title with and without the number of
-                        // new messages included in the title.
-                        var timeoutFn = function () {
-                            timerId = $timeout(function () {
-                                if (numMessagesIsShownToggle) {
-                                    document.title = $('#id-document-title-div').text();
-
-                                } else {
-                                    // the arrow is now shown, display it for a while
-                                    document.title = '(' + numMessagesReceivedSinceWindowLastActive + ') ' + $('#id-document-title-div').text();
-                                }
-                                numMessagesIsShownToggle = !numMessagesIsShownToggle;
-                                // after initial wait, start flashing every 2 seconds.
-                                timeoutDelay = 1000;
-
-                                timeoutFn();
-                            }, timeoutDelay);
-                        };
-                        timeoutFn();
-                    }
-                    // otherwise the user is currently looking at the window, and so we make sure that the
-                    // display does not show number of messages and that the timer and counter are reset.
-                    else {
-                        resetDocumentTitleToDefault();
-                    }
-                };
-
                 // watch to see if the local user has sent a new chat message to the remote user
                 scope.$watch('sendMessageStringTime', function() {
                     if (scope.sendMessageString) {
@@ -151,12 +152,7 @@ angular.module('lxChatbox.directives', [])
                     }
                 });
 
-                var timerId;
-
-
-
-
-                scope.$watch('chatMessageObject.receivedMessageStringTime', function(newValue) {
+                scope.$watch('chatMessageObject.receivedMessageStringTime', function() {
                     if (scope.chatMessageObject.receivedMessageString) {
                         addMessageToDisplay(scope.chatMessageObject.receivedMessageString, 'right', true);
                         numMessagesReceivedSinceWindowLastActive++;
