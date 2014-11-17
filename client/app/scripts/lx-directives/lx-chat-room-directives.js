15806c2067e7820e339b01b1dfe57513916ea316
Alexander Marquardt <alexander.marquardt@gmail.com>
2014-11-17 11:32:21 -0500

    Split initializeChannelAndTurn directive into two seperate directives.

--- client/app/scripts/lx-directives/lx-chat-room-directives.js
+++ client/app/scripts/lx-directives/lx-chat-room-directives.js
@@ -4,17 +4,13 @@
 
 angular.module('lxChatRoom.directives', [])
 
-    .directive('lxInitializeChannelAndTurnDirective',
+    .directive('lxInitializeChannelDirective',
 
     function(
         $log,
         $window,
         lxChannelService,
-        lxChannelSupportService,
-        lxHttpChannelService,
-        lxWebRtcSessionService,
-        lxUseChatRoomVarsService,
-        lxTurnService
+        lxHttpChannelService
         ) {
 
 
@@ -26,7 +22,6 @@ angular.module('lxChatRoom.directives', [])
                 // Setup the channel and turn. If no exceptions are found returns true, otherwise false
                 try {
                     lxChannelService.openChannel(scope);
-                    lxTurnService.maybeRequestTurn();
 
                     $window.onbeforeunload = function () {
                         $log.debug('Manually disconnecting channel on window unload event.');
@@ -37,12 +32,33 @@ angular.module('lxChatRoom.directives', [])
                     return true;
                 }
                 catch (e) {
-                    e.message = '\n\tError in lxInitializeChannelAndTurnDirective\n\t' + e.message;
+                    e.message = '\n\tError in lxInitializeChannelDirective\n\t' + e.message;
                     $log.error(e);
                     return false;
                 }
             }
         };
+    })
+
+    .directive('lxInitializeTurnDirective',
+    function(
+        $log,
+        lxTurnService
+        ) {
+
+        return {
+            restrict: 'A',
+            link: function(scope) {
+                try {
+                    lxTurnService.maybeRequestTurn();
+                }
+                catch (e) {
+                    e.message = '\n\tError in lxInitializeTurnDirective\n\t' + e.message;
+                    $log.error(e);
+                    return false;
+                }
+            }
+        }
     })
 
     // Directive that will control signalling between two users about the type of video that they wish to transmit.
