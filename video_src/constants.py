937e06f54345d09279e9ceade04f17f5c2bceea5
Alexander Marquardt <alexander.marquardt@gmail.com>
2015-03-19 15:41:10 -0400

    Shortened maximum room name length.

--- video_src/constants.py
+++ video_src/constants.py
@@ -6,7 +6,7 @@ site_name_dot_com = 'chatsurfing.com'
 site_name_for_display = 'ChatSurfing'
 
 room_min_chars = 3
-room_max_chars = 80
+room_max_chars = 20
 room_max_occupancy = 100 # arbitrarily limit the number of people in a chat room
 
 """"
