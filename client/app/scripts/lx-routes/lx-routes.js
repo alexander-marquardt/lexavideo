38c1188e9647784c5c5eef582a4793f4b2f2f322
Alexander Marquardt <alexander.marquardt@gmail.com>
2015-03-21 13:40:24 -0400

    Added a dummy chatbox view to give the sensation of faster response when creating a new chatbox.

--- client/app/scripts/lx-routes/lx-routes.js
+++ client/app/scripts/lx-routes/lx-routes.js
@@ -19,7 +19,9 @@ lxMainRoutes.config(function ($routeProvider, $locationProvider) {
         /* When chatbox URLs are selected,  the ngView that is shown is blank and one of
          the chat panels defined in the lx-chatbox.html file will be enabled, depending on the
          current URL (where the URL contains the chat name). */
-        template: '',
+        templateUrl: function(){
+                return '/_lx/lx-templates/lx-dummy-chatbox-view.html';
+            },
         controller: 'lxChatViewCtrl'
     });
 
