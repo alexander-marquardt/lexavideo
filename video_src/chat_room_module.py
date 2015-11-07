152a1585a27bdb58e2b45b3086ae38c741e4b540
Alexander Marquardt <alexander.marquardt@gmail.com>
2015-11-07 19:13:29 +0000

    Added license information to the python files.

--- video_src/chat_room_module.py
+++ video_src/chat_room_module.py
@@ -1,3 +1,27 @@
+# LexaVideo Copyright information - do not remove this copyright notice
+# Copyright (C) 2015 - Alexander Marquardt
+#
+# LexaVideo -  a fully responsive web-app featuring real-time browser-based video conferencing and text chat.
+#
+# Original author: Alexander Marquardt
+# Documentation and additional information: http://www.lexavideo.com
+# A demo version of LexaVideo can be seen at http://www.chatsurfing.com
+#
+# Please consider contributing your enhancements and modifications to the LexaVideo community.
+# Git source code repository: https://github.com/alexander-marquardt/lexavideo
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 import copy
 import json
