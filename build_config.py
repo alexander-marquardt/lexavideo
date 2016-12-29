5798a313d28a7fbc3a23f2989e1fedf592831644
Alexander Marquardt <alexander.marquardt@gmail.com>
2016-12-29 14:00:05 +0100

    Updated to a newer version of the adapter.js shim. This fixes some issues with newer browsers

--- build_config.py
+++ build_config.py
@@ -3,7 +3,7 @@
 import os, datetime, re, codecs, subprocess, logging
 
 
-VERSION_ID = "2014-09-12-0324"
+VERSION_ID = "2016-12-29-1204"
 
 # The following must be set to False before uploading - this will combine and minimize javascript 
 # and css files. This combining/minimizing is only done on upload or on  development server initialization, so this will
